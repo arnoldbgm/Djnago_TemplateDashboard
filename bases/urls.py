513b6c815f458e5e270ac2e8356a33517b948076
arnoldbgm <arnold.gallegosm@gmail.com>
2024-07-20 14:52:19 -0500

    Feat: Login y view Login

--- bases/urls.py
+++ bases/urls.py
@@ -1,7 +1,10 @@
-# urls.py
 from django.urls import path
 from .views import Home
 
+# Ahora hare un login usando el LoginView que es propio de django
+from django.contrib.auth import views as auth_view
+
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('login/', auth_view.LoginView.as_view(template_name="bases/login.html"), name="login")
 ]
--- bases/views.py
+++ bases/views.py
@@ -1,13 +1,9 @@
 # views.py
 from django.shortcuts import render
 from django.views import generic
+# Este mixin va actuar como un midleware el cual exigira que este logeando antes de que pueda ver la vista
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-# Aquí vamos a crear nuestra vista
-
-
-class Home(generic.TemplateView):
-    # Con usar template_name va a renderizar la plantilla
-    #  template_name = 'base/base.html'
-    # Ahora que cree mi home le dire que me redireccione a donde a mi home.html
+class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
+    # Si no esta registrado mi usario le dire que se vaya al ..
+    login_url='/admin'
